d154ff9fb6e118bad2492cb1e7c8a671c9cb0b9f
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-10-18 11:26:23 +0200

    feat: fontawesome npm, font-family

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 import App from './App'
 
+library.add(fab, faCheckSquare, faCoffee)
 const rootElement = document.getElementById('root');
 
 if (rootElement) [ReactDOM.render(<App />, rootElement)];
