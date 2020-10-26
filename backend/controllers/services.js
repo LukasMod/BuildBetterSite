b1c5d13ccd1522e64c8c41c0fcd09f40a51c75e0
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-10-26 08:40:35 +0100

    feat: service images

--- backend/controllers/services.js
+++ backend/controllers/services.js
@@ -2,49 +2,53 @@ const { v4: uuid } = require('uuid');
 
 const servicesData = [
   {
-    description: 'Wall on a 75mm profile, double-sided normal plasterboard 2 x 12.5 mm',
+    description:
+      'Wall on a 75mm profile, double-sided normal plasterboard 2 x 12.5 mm',
     id: uuid(),
-    img: '',
+    img: '../img/plasterboardN300x240.png',
     price: 200,
     producers: ['Siniat, Norgips, Saint-Gobain'],
     title: 'Plasterboard system wall 125mm - normal',
   },
   {
-    description: 'Wall on a 75mm profile, double-sided water resistant plasterboard 2 x 12.5 mm',
+    description:
+      'Wall on a 75mm profile, double-sided water resistant plasterboard 2 x 12.5 mm',
     id: uuid(),
-    img: '',
+    img: '../img/plasterboardW300x240.png',
     price: 230,
     producers: ['Siniat, Norgips, Saint-Gobain'],
     title: 'Plasterboard system wall 125mm - water resistant',
   },
   {
-    description: 'Wall on a 75mm profile, double-sided silent plasterboard 2 x 12.5 mm',
+    description:
+      'Wall on a 75mm profile, double-sided silent plasterboard 2 x 12.5 mm',
     id: uuid(),
-    img: '',
+    img: '../img/plasterboardS300x240.png',
     price: 240,
     producers: ['Siniat, Norgips, Saint-Gobain'],
     title: 'Plasterboard system wall 125mm - silent',
   },
   {
-    description: 'Wall on a 75mm profile, double-sided fire resistant plasterboard 2 x 12.5 mm',
+    description:
+      'Wall on a 75mm profile, double-sided fire resistant plasterboard 2 x 12.5 mm',
     id: uuid(),
-    img: '',
+    img: '../img/plasterboardF300x240.png',
     price: 250,
     producers: ['Siniat, Norgips, Saint-Gobain'],
     title: 'Plasterboard system wall 125mm - fire resistant',
-  }
-
+  },
 ];
 
 exports.getServices = (request, response, next) => {
   try {
     response.status(200).json({
-      services: servicesData
+      services: servicesData,
     });
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /services',
+      message:
+        'Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /services',
     });
   }
 };
@@ -52,7 +56,7 @@ exports.getServices = (request, response, next) => {
 exports.getService = (request, response, next) => {
   try {
     const { id } = request.params;
-    const serviceToSend = servicesData.find(service => service.id === id);
+    const serviceToSend = servicesData.find((service) => service.id === id);
 
     if (!serviceToSend) {
       response.status(404).json({
@@ -68,8 +72,9 @@ exports.getService = (request, response, next) => {
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /services/:id',
-    })
+      message:
+        'Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /services/:id',
+    });
   }
 };
 
@@ -78,13 +83,16 @@ exports.postService = (request, response, next) => {
     const { description, img, price, producers, title } = request.body;
     if (!description || !price || !producers || !title) {
       response.status(400).json({
-        message: 'Nie podano wszystkich wymaganych informacji (opis, producent, cena, tytuł)',
+        message:
+          'Nie podano wszystkich wymaganych informacji (opis, producent, cena, tytuł)',
       });
 
       return;
     }
 
-    const isServiceExist = servicesData.some(({ title: currentTitle }) => currentTitle === title);
+    const isServiceExist = servicesData.some(
+      ({ title: currentTitle }) => currentTitle === title
+    );
     if (isServiceExist) {
       response.status(409).json({
         message: `Istnieje już w bazie usługa: ${title}`,
@@ -105,12 +113,13 @@ exports.postService = (request, response, next) => {
     servicesData.push(newService);
 
     response.status(201).json({
-      services: servicesData
+      services: servicesData,
     });
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie POST w endpointcie /services'
+      message:
+        'Oops! Coś poszło nie tak, przy metodzie POST w endpointcie /services',
     });
   }
 };
@@ -120,13 +129,16 @@ exports.putService = (request, response, next) => {
     const { description, id, price, producers, title } = request.body;
     if (!description || !id || !price || !producers || !title) {
       response.status(400).json({
-        message: 'Nie podano wszystkich wymaganych informacji (opis, id, cena, producent, tytuł)',
+        message:
+          'Nie podano wszystkich wymaganych informacji (opis, id, cena, producent, tytuł)',
       });
 
       return;
     }
 
-    const indexServiceToUpdate = servicesData.findIndex(service => service.id === id);
+    const indexServiceToUpdate = servicesData.findIndex(
+      (service) => service.id === id
+    );
     if (indexServiceToUpdate === -1) {
       response.status(404).json({
         message: 'Nie znaleziono kursu o podanym id',
@@ -135,16 +147,16 @@ exports.putService = (request, response, next) => {
       return;
     }
 
-
     servicesData.splice(indexServiceToUpdate, 1, request.body);
 
     response.status(202).json({
-      services: servicesData
+      services: servicesData,
     });
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie PUT w endpointcie /services'
+      message:
+        'Oops! Coś poszło nie tak, przy metodzie PUT w endpointcie /services',
     });
   }
 };
@@ -154,7 +166,9 @@ exports.deleteService = (request, response, next) => {
     const { id } = request.params;
 
     console.log(id);
-    const indexServiceToDelete = servicesData.findIndex(service => service.id === id);
+    const indexServiceToDelete = servicesData.findIndex(
+      (service) => service.id === id
+    );
 
     if (indexServiceToDelete === -1) {
       response.status(404).json({
@@ -169,7 +183,8 @@ exports.deleteService = (request, response, next) => {
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie DELETE w endpointcie /services/:id',
+      message:
+        'Oops! Coś poszło nie tak, przy metodzie DELETE w endpointcie /services/:id',
     });
   }
 };
